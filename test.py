18437cbce4a7075c855cb7772398c17f395a6b08
Charles Lavery <clavery@salesforce.com>
2025-02-07 14:28:12 -0500

    test: Add test.py file

---
+++ test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env -S uv run --script
+# /// script
+# requires-python = ">=3.12"
+# dependencies = [
+#   "requests"
+# ]
+# ///
+
+import requests
+
+
+# get a list of sites using ./data/sites.yaml resource AI!
+
